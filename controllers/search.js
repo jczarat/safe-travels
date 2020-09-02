980c8b480cabb9e628dc1f47c7309d0b03ae4843
Jesse Zarat <jczarat@gmail.com>
2020-09-02 11:09:49 -0600

    begin css on show page

--- controllers/search.js
+++ controllers/search.js
@@ -14,7 +14,6 @@ function index(req, res){
         let countriesArray = Object.values(dataBody.data);
         let searchedTerm = req.query.name;
         let searchResult = countriesArray.find(country => country.name === searchedTerm)
-        console.log(searchResult);
         res.render('search/index', {title: 'All Countries', searchResult, searchedTerm});
     })
 }
